e64449bff6df617e01e64eb43dd08a21c5474373
kamenkri <Kristiyan.Kamenov@externals.adidas.com>
2020-01-03 11:24:17 +0200

    algolia setup

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,37 @@ admin.initializeApp();
 
 const db = admin.firestore();
 
+const algoliasearch = require('algoliasearch');
+
+const APP_ID = functions.config().algolia.app;
+const ADMIN_KEY = functions.config().algolia.key;
+
+const client = algoliasearch(APP_ID, ADMIN_KEY);
+const index = client.initIndex('applications');
+
+exports.addToIndex = functions
+  .firestore
+  .document('applications/{applicationId}')
+  .onCreate((snapshot) => {
+    const data = snapshot.data();
+    const objectID = snapshot.id;
+    return index.addObject({ ...data, objectID });
+  });
+
+exports.updateIndex = functions
+  .firestore
+  .document('applications/{applicationId}')
+  .onUpdate((change) => {
+    const newData = change.after.data();
+    const objectID = change.after.id;
+    return index.addObject({ ...newData, objectID });
+  });
+
+exports.deleteFromIndex = functions
+  .firestore
+  .document('applications/{applicationId}')
+  .onDelete((snapshot) => index.deleteObject(snapshot.id));
+
 exports.onApplicationCreate = functions
   .firestore
   .document('applications/{applicationId}')
