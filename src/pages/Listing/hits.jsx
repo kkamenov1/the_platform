9715ed69c5691101b0f14697624444123fc7db7b
kamenkri <Kristiyan.Kamenov@externals.adidas.com>
2020-02-19 14:44:26 +0200

    no search results added

--- src/pages/Listing/hits.jsx
+++ src/pages/Listing/hits.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import {
   Divider,
   Grid,
+  Typography,
 } from '@material-ui/core';
 import { connectHits } from 'react-instantsearch-dom';
 import Hit from './hit';
@@ -12,15 +13,29 @@ const Hits = ({
   onHitOver,
   showMap,
 }) => (
-  <Grid container spacing={!showMap ? 4 : 0}>
-    {showMap && <Divider />}
-    {hits.map((hit, i) => (
-      <Grid key={hit.objectID + i} item xs={showMap ? 12 : 3}>
-        <Hit hit={hit} onHitOver={onHitOver} showMap={showMap} />
+  <>
+    {hits && hits.length ? (
+      <Grid container spacing={!showMap ? 4 : 0}>
         {showMap && <Divider />}
+        {hits.map((hit, i) => (
+          <Grid key={hit.objectID + i} item xs={showMap ? 12 : 3}>
+            <Hit hit={hit} onHitOver={onHitOver} showMap={showMap} />
+            {showMap && <Divider />}
+          </Grid>
+        ))}
       </Grid>
-    ))}
-  </Grid>
+    ) : (
+      <>
+        <Typography variant="h6" component="h6" paragraph>
+          No results found
+        </Typography>
+        <Typography variant="body2" component="p" paragraph>
+          No gurus found for that location. Maybe you can search for other location.
+        </Typography>
+        <Divider />
+      </>
+    )}
+  </>
 );
 
 Hits.defaultProps = {
